4c8050fa0acc6a70dbe6d65345cb7973ba1a8624
Michael <Michael@DESKTOP-D0G6QQV>
2017-04-30 14:16:46 +0300

    +autoload.php

--- index.php
+++ index.php
@@ -1,13 +1,6 @@
 <?php
-
 error_reporting(-1);
-//Автозагрузка
-spl_autoload_register(function ($class) {
-    $file = __DIR__ . '/' . str_replace('\\', '/', $class) . '.php';
-    if (file_exists($file)) {
-        include $file;
-    }
-});
+include __DIR__ . '/autoload.php';
 
 use MyClass\Db;
 
@@ -20,7 +13,7 @@ try {
     exit($e->getMessage());
 }
 /**
- * @todo заменить на более 'адекватное' решение стр. 25 - 61
+ * @todo заменить
  */
 if (isset($_GET['id'])) {
     $action = strip_tags($_GET['id']);
@@ -31,40 +24,27 @@ if (isset($_GET['id'])) {
             $email = trim(strip_tags($_POST['email']));
         if (isset($_POST['id']))
             $id = (int) $_POST['id'];
-        switch ($action) {
-            case 'add':
-                try {
+        try {
+            switch ($action) {
+                case 'add':
                     $result = $test->addUser($name, $email);
-                } catch (Exception $e) {
-                    $result = $e->getMessage();
-                }
-                break;
-            case 'get':
-                try {
+                    break;
+                case 'get':
                     $result = $test->getUserData($id);
-                } catch (Exception $e) {
-                    $result = $e->getMessage();
-                }
-                break;
-            case 'update':
-                try {
+                    break;
+                case 'update':
                     $updateData = $test->updateData($id, $name, $email);
-                    if ($updateData) {
+                    if ($updateData)
                         $result = $test->getUserData($id);
-                    }
-                } catch (Exception $e) {
-                    $result = $e->getMessage();
-                }
-                break;
-            default:
-                break;
+                    break;
+                default:
+                    break;
+            }
+        } catch (Exception $e) {
+            $result = $e->getMessage();
         }
     }
 } else {
     $action = '';
 }
 include __DIR__ . '/Templates/index.php';
-
-
-
-
